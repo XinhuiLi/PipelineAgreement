d2863cd781ba3a34d6eb14dcdd189bed9735b088
Xinhui Li <sucyli1017@gmail.com>
2021-11-09 09:25:30 -0500

    add code to validate fig5

---
+++ figure/5/corr_ts.py
@@ -0,0 +1,62 @@
+import os
+import math
+import numpy as np
+import nibabel as nb
+
+resolution = 'native' #'2mm'
+dataout = '/data3/cnl/xli/reproducibility/correlation'
+if resolution == '2mm':
+    img3d = '/data3/cnl/xli/reproducibility/out/fmriprep/fmriprep_mni152_2mm/output/fmriprep/sub-0025427a/func/sub-0025427a_task-rest_run-1_space-MNI152NLin6Asym_res-2_desc-mean_brain_bold.nii.gz'
+else:
+    img3d = '/data3/cnl/xli/reproducibility/out/fmriprep/fmriprep_mni2004_native/output/fmriprep/sub-0025427a/func/sub-0025427a_task-rest_run-1_space-MNI1522004_desc-mean_brain_bold.nii.gz'
+
+for s, subid in enumerate(range(27,57)):
+    sub = 'sub-00254'+str(subid)+'a'
+    print(sub)
+
+    corr2001_2006 = np.zeros( ( 91, 109, 91 ) )
+    corr2001_2009 = np.zeros( ( 91, 109, 91 ) )
+    corr2006_2009 = np.zeros( ( 91, 109, 91 ) )
+
+    if resolution == '2mm':
+        t2001_path='/data3/cnl/xli/reproducibility/out/fmriprep/fmriprep_mni2004_2mm/output/fmriprep/'+sub+'/func/'+sub+'_task-rest_run-1_space-MNI1522004_res-2_desc-resampled_brain_bold_lpi.nii.gz'
+        t2006_path='/data3/cnl/xli/reproducibility/out/fmriprep/fmriprep_mni152_2mm/output/fmriprep/'+sub+'/func/'+sub+'_task-rest_run-1_space-MNI152NLin6Asym_res-2_desc-brain_bold.nii.gz'
+        t2009_path='/data3/cnl/xli/reproducibility/out/fmriprep/fmriprep_mni2009_2mm/output/fmriprep/'+sub+'/func/'+sub+'_task-rest_run-1_space-MNI152NLin2009cAsym_res-2_desc-resampled_brain_bold_lpi.nii.gz'
+    else:
+        t2001_path='/data3/cnl/xli/reproducibility/out/fmriprep/fmriprep_mni2004_native/output/fmriprep/'+sub+'/func/'+sub+'_task-rest_run-1_space-MNI1522004_desc-brain_bold.nii.gz'
+        t2006_path='/data3/cnl/xli/reproducibility/out/fmriprep/fmriprep_mni152_native/output/fmriprep/'+sub+'/func/'+sub+'_task-rest_run-1_space-MNI152NLin6Asym_desc-brain_bold.nii.gz'
+        t2009_path='/data3/cnl/fmriprep/Lei_working/FINAL_preprocessed_2021/fmriprep_default/output/fmriprep/'+sub+'/func/'+sub+'_task-rest_run-1_space-MNI152NLin2009cAsym_desc-resampled_brain_bold.nii.gz'
+
+    t2001 = nb.load(t2001_path).get_data()
+    t2006 = nb.load(t2006_path).get_data()
+    t2009 = nb.load(t2009_path).get_data()
+
+    for x in range(91):
+        for y in range(109):
+            for z in range(91):
+                corr2001_2006[x, y, z] = np.corrcoef(t2001[x, y, z], t2006[x, y, z])[1,0]
+                if math.isnan(corr2001_2006[x,y,z]):
+                    corr2001_2006[x, y, z] = 0
+                # print('2001 vs 2006 completed')
+                
+                corr2001_2009[x, y, z] = np.corrcoef(t2001[x, y, z], t2009[x, y, z])[1,0]
+                if math.isnan(corr2001_2009[x,y,z]):
+                    corr2001_2009[x, y, z] = 0
+                # print('2001 vs 2009 completed')
+
+                corr2006_2009[x, y, z] = np.corrcoef(t2006[x, y, z], t2009[x, y, z])[1,0]
+                if math.isnan(corr2006_2009[x,y,z]):
+                    corr2006_2009[x, y, z] = 0
+                # print('2006 vs 2009 completed')
+
+    output_img = nb.Nifti1Image(corr2001_2006, affine=nb.load(img3d).get_affine())
+    out_file = os.path.join(dataout, '2001_2006_'+resolution, sub+'.nii.gz')
+    output_img.to_filename(out_file)
+
+    output_img = nb.Nifti1Image(corr2001_2009, affine=nb.load(img3d).get_affine())
+    out_file = os.path.join(dataout, '2001_2009_'+resolution, sub+'.nii.gz')
+    output_img.to_filename(out_file)
+
+    output_img = nb.Nifti1Image(corr2006_2009, affine=nb.load(img3d).get_affine())
+    out_file = os.path.join(dataout, '2006_2009_'+resolution, sub+'.nii.gz')
+    output_img.to_filename(out_file)
